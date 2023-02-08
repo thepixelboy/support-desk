c782599ab51a7e9d1db182c372c2a48b9ccdffab
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-08 16:31:48 +0100

    feat: basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
