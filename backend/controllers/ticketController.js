dfe0c750ef9a414bef462ca7d55015c0dbcff4b7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-13 09:23:01 +0100

    feat: single ticket, update and delete

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -20,6 +20,34 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// @desc Get user ticket
+// @route GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+  // get user using id from token
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // get tickets using user id
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== user._id.toString()) {
+    res.status(401);
+    throw new Error("Unauthorized");
+  }
+
+  res.status(200).json(ticket);
+});
+
 // @desc Create a new ticket
 // @route POST /api/tickets
 // @access Private
@@ -49,4 +77,74 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTicket };
+// @desc Delete ticket
+// @route DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  // get user using id from token
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // get tickets using user id
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== user._id.toString()) {
+    res.status(401);
+    throw new Error("Unauthorized");
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+// @desc Update ticket
+// @route PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // get user using id from token
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // get tickets using user id
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== user._id.toString()) {
+    res.status(401);
+    throw new Error("Unauthorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
+module.exports = {
+  getTickets,
+  getTicket,
+  createTicket,
+  deleteTicket,
+  updateTicket,
+};
