77675e04aaa205c8d86a593773b37d4120248727
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-13 10:59:41 +0100

    feat: add tickets to redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import authReducer from "../features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import ticketReducer from "../features/tickets/ticketSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    ticket: ticketReducer,
   },
 });
